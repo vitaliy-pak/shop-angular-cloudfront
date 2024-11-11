76564474a8caacafa1498f838132793452e3bf36
Vitaliy Pak <vitaliy_pak@epam.com>
2024-11-11 12:42:57 +0500

    feat: show custom message for 401 and 403 errors

--- src/app/admin/manage-products/manage-products.service.ts
+++ src/app/admin/manage-products/manage-products.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { EMPTY, Observable } from 'rxjs';
 import { ApiService } from '../../core/api.service';
 import { switchMap } from 'rxjs/operators';
+import { HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class ManageProductsService extends ApiService {
@@ -27,8 +28,15 @@ export class ManageProductsService extends ApiService {
 
   private getPreSignedUrl(fileName: string): Observable<string> {
     const url = this.getUrl('import', 'import');
+    const authorizationToken = localStorage.getItem('authorization_token');
+    let headers = new HttpHeaders();
+
+    if (authorizationToken) {
+      headers = headers.set('Authorization', `Basic ${authorizationToken}`);
+    }
 
     return this.http.get<string>(url, {
+      headers: headers,
       params: {
         name: fileName,
       },
